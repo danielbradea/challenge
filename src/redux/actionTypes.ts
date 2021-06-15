386bd52455b589fd045f7f9350590bd2f0a52f18
Daniel Bradea <daniel.bradea@ferendia.com>
2021-06-15 09:35:35 +0300

    first commit

---
+++ src/redux/actionTypes.ts
@@ -0,0 +1,2 @@
+export const ADD_EVENT = 'REGISTER'
+export const DELETE_EVENT = 'REGISTER'
---
+++ src/redux/actions.ts
@@ -0,0 +1,8 @@
+import {ADD_EVENT,DELETE_EVENT} from "./actionTypes"
+
+export function addEvent(){
+    return {type: ADD_EVENT}
+}
+export function deleteEvent(){
+    return {type: DELETE_EVENT}
+}
